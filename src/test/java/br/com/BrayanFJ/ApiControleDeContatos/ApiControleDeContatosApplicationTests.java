028ff390cb0bd7a020e65bd198f24fcf035292de
BrayanFJ <Brayan.fernandesjulio@gmail.com>
2025-02-10 21:27:40 -0300

    Inicialização do projeto API Controle de Contatos

---
+++ src/test/java/br/com/BrayanFJ/ApiControleDeContatos/ApiControleDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.BrayanFJ.ApiControleDeContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
