6fff03787cfcba8b725b1be0da01aac4513ad99d
BrayanFJ <Brayan.fernandesjulio@gmail.com>
2025-02-17 14:35:17 -0300

    Criação do endpoint GET para buscar um contato por ID

--- src/main/java/br/com/BrayanFJ/ApiControleDeContatos/resource/ContatoResource.java
+++ src/main/java/br/com/BrayanFJ/ApiControleDeContatos/resource/ContatoResource.java
@@ -3,6 +3,7 @@ package br.com.BrayanFJ.ApiControleDeContatos.resource;
 import br.com.BrayanFJ.ApiControleDeContatos.model.Contato;
 import br.com.BrayanFJ.ApiControleDeContatos.service.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,13 @@ public class ContatoResource {
     @GetMapping
     public List<Contato> listarContatos() {
         return contatoService.listarTodosContatos();
+    }
+
+    // Endpoint para buscar um contato por ID (CRUD - GET / este busca por um id especifico )
+    @GetMapping("/{id}")
+    public ResponseEntity<Contato> buscarContatoPorId(@PathVariable Long id) {
+        Optional<Contato> contato = contatoService.buscarContatoPorId(id);
+        return contato.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 
