549639d01db26244ad321c40e3df933a124decf7
BrayanFJ <Brayan.fernandesjulio@gmail.com>
2025-02-17 13:40:50 -0300

    Criação da classe ContatoResource e do endpoint post

---
+++ src/main/java/br/com/BrayanFJ/ApiControleDeContatos/resource/ContatoResource.java
@@ -0,0 +1,24 @@
+package br.com.BrayanFJ.ApiControleDeContatos.resource;
+
+import br.com.BrayanFJ.ApiControleDeContatos.model.Contato;
+import br.com.BrayanFJ.ApiControleDeContatos.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/contatos") // Mapeia a URL para esta classe
+public class ContatoResource {
+
+    @Autowired
+    private ContatoService contatoService;
+
+    // Endpoint para criar um novo contato (CRUD - POST)
+    @PostMapping
+    public Contato criarContato(@RequestBody Contato contato) {
+        return contatoService.salvarContato(contato);
+    }
+
+}
